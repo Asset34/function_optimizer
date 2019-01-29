a2fd2318407ff966df4c862d398058aa9d723950
Asset34 <artemiy.kashintsev@gmail.com>
2019-01-29 21:41:10 +0300

    RandomGenerator: update

--- src/utills/randomgenerator.cpp
+++ src/utills/randomgenerator.cpp
@@ -1,13 +1,14 @@
 #include "randomgenerator.hpp"
 
 #include <algorithm>
+#include <numeric>
 #include <chrono>
 
-//std::default_random_engine RandomGenerator::m_engine(
-//        std::chrono::system_clock::now().time_since_epoch().count()
-//        );
+std::default_random_engine RandomGenerator::m_engine(
+        std::chrono::system_clock::now().time_since_epoch().count()
+        );
 
-std::default_random_engine RandomGenerator::m_engine;
+//std::default_random_engine RandomGenerator::m_engine;
 std::uniform_int_distribution<int> RandomGenerator::m_boolDistr(0, 1);
 
 bool RandomGenerator::generateBool()
@@ -51,26 +52,17 @@ int RandomGenerator::generateRoulette(const std::vector<double> &sectors)
 
 std::vector<int> RandomGenerator::generateSet(int count, int min, int max)
 {
-    // Create range vector
     std::vector<int> values(max - min + 1);
-    for (int i = 0; i < values.size(); i++) {
-        values[i] = min + i;
-    }
 
-    // Generate
+    std::iota(values.begin(), values.end(), min);
     std::shuffle(values.begin(), values.end(), m_engine);
-    if (count) {
-        values.resize(count);
-    }
+    values.resize(count);
 
     return values;
 }
 
 bool RandomGenerator::generateAction(double chance)
 {
-    std::uniform_real_distribution<double> distr(0.0, 1.0);
-
-//    double result = distr(m_engine);
     double result = generateDouble(0.0, 1.0);
     if (result <= chance) {
         return true;
