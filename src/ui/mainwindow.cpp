8ed520db9b1aed9465278ebe4b7a299396626d5b
Asset34 <artemiy.kashintsev@gmail.com>
2019-02-01 16:29:26 +0300

    MainWindow: partially implement class

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,7 +1,26 @@
 #include "mainwindow.hpp"
 
+#include "widgets/functionoptimizerwidget.hpp"
+
+#include <QStatusBar>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    m_optimizerWidget = new FunctionOptimizerWidget;
+
+    setCentralWidget(m_optimizerWidget);
+    setContentsMargins(5, 5, 5, 5);
+
+    connect(
+        m_optimizerWidget,
+        &FunctionOptimizerWidget::statusChanged,
+        this,
+        &MainWindow::indicateSolverStatus
+        );
 }
 
+void MainWindow::indicateSolverStatus(QString status)
+{
+    statusBar()->showMessage(status);
+}
--- src/ui/mainwindow.hpp
+++ src/ui/mainwindow.hpp
@@ -3,14 +3,22 @@
 
 #include <QMainWindow>
 
+#include <QString>
+
+class FunctionOptimizerWidget;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
 
 private:
+    FunctionOptimizerWidget *m_optimizerWidget;
+
+private slots:
+    void indicateSolverStatus(QString status);
 
 };
 
